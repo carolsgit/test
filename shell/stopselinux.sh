d941c983e99620663ac3c5333244687a4384e77e
sunny <9016178@qq.com>
2018-05-11 09:27:46 +0800

    add stopselinux.sh

---
+++ shell/stopselinux.sh
@@ -0,0 +1,24 @@
+﻿关闭iptables和selinux
+
+
+#！/bin/bash
+#关闭iptables和selinux
+
+
+#iptables stop
+/etc/init.d/iptables stop
+/sbin/chkconfig iptables off
+
+
+#selinux stop
+sed  -i.ori s#^SELINUX=enforcing#SELINUX=disabled#g' /etc/selinux/config 
+sleep 1
+setenforce 0
+sleep 1
+getenforce
+
+
+
+
+
+
